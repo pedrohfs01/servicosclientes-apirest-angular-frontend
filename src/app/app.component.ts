f75cf0ff26190f7608f92b34a335a979afb0b9d7
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-18 10:51:58 -0300

    Criar toggle sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core'
+
+import jQuery from 'jquery';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ServicosCliente-app';
+
+  ngAfterViewInit() {
+    (function ($) {
+      "use strict";
+
+      var path = window.location.href;
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+          $(this).addClass("active");
+        }
+      });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
 }
