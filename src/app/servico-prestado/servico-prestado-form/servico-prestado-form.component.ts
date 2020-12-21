ffec46b85e4be52e2add9ebec531e8f9730915c1
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-21 14:00:48 -0300

    Criar primeira parte de serviços prestados

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/clientes/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoFormComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente[] = [];
+
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
+
+    this.clienteService.getClientes().subscribe(response => this.clientes = response);
+
   }
 
+  onSubmit(){
+    
+  }
 }
