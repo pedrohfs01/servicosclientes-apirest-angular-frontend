c2cd7a76585009e6e3e59d2458df5d0c8e4a7887
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-22 08:38:29 -0300

    Adicionar formulário de servico prestado

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/clientes/cliente';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { ServicoPrestado } from '../servico-prestado';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -11,15 +12,19 @@ export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = [];
 
-  constructor(private clienteService: ClienteService) { }
+  public servico: ServicoPrestado;
+
+  constructor(private clienteService: ClienteService) { 
+    this.servico = new ServicoPrestado();
+  }
 
   ngOnInit(): void {
-
     this.clienteService.getClientes().subscribe(response => this.clientes = response);
-
+    
   }
 
   onSubmit(){
+    console.log(this.servico);
     
   }
 }
---
+++ src/app/servico-prestado/servico-prestado.ts
@@ -0,0 +1,7 @@
+export class ServicoPrestado{
+
+    private descricao: string;
+    private data: string;
+    private preco: string;
+    private idCliente: number;
+}
