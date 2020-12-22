cb1e012bc04cc2dd15e2e81577ac872bbeb11113
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-22 13:06:26 -0300

    Separar login do template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,12 @@
-import { AfterViewInit, Component } from '@angular/core'
+import { Component } from '@angular/core';
 
-import jQuery from 'jquery';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent{
+
   title = 'Serviços Cliente';
 
-  ngAfterViewInit() {
-    (function ($) {
-      "use strict";
-
-      var path = window.location.href;
-      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
-        if (this.href === path) {
-          $(this).addClass("active");
-        }
-      });
-
-      // Toggle the side navigation
-      $("#sidebarToggle").on("click", function (e) {
-        e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
-      });
-    })(jQuery);
-  }
 }
