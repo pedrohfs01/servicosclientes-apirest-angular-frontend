00e2c956ba8fd7a5f9396bfe55981522450583f6
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-24 12:49:54 -0300

    Interceptar requisições para passar o token!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { LayoutComponent } from './layout/layout.component';
 import { AuthService } from './services/auth.service';
+import { TokenInterceptor } from './token.interceptor';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { AuthService } from './services/auth.service';
   providers: [
     ClienteService,
     ServicoPrestadoService,
-    AuthService
+    AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass:TokenInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
